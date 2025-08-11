9d86dcfe71c753dc0aa00dcec33fc1ee7c11ed44
Mina Dang <md2327@users noreply.github.com>
2025-08-11 02:50:17 -0500

    Updated ProjectCard.module.css

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -7,31 +7,26 @@ export const ProjectCard = ({
 }) => {
   return (
     <div className={styles.container}>
-      <img
+      <div
         className={styles.image}
-        src={getImageUrl(imageSrc)}
         alt={`Image of ${title}`}
-      />
-      <div className={styles.content}>
+        style={{
+        backgroundImage: `url(${getImageUrl(imageSrc)})`}}
+      >
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.description}>{description}</p>
-        <ul className={styles.skills}>
-          {skills.map((skill, id) => {
-            return (
-              <li key={id} className={styles.skill}>
-                {skill}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className={styles.link}>
-        <a href={demo} className={styles.demo}>
-          Demo
-        </a>
-        <a href={source} className={styles.source}>
-          Source
-        </a>
+              <ul className={styles.skills}>
+                {skills.map((skill, id) => {
+                  return (
+                    <li key={id} className={styles.skill}>
+                      {skill}
+                    </li>
+                  );
+                })}
+              </ul>
+              <a href={demo} className={styles.demo}>
+                Demo
+              </a>
       </div>
     </div>
   );
